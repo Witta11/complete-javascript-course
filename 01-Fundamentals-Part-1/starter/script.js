119ceb890f85c9cf48933ee3f6c7d6cffd2adc87
Witta Glöckner <alina.glocke@gmail.com>
2020-12-15 10:21:14 +0100

    basic operators

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+let js = 'amazing';
+// if (js === 'amazing') alert("JavaScript is FUN!")
+
+console.log(4 + 4 * 20);
+
+let firstName = 'Witta'
+console.log(firstName)
+
+// lower camel case convention
+// some constants in uppercase
+
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let country = "Germany";
+let continent = "Europe";
+let population = 8.8
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+
+/*
+thats multi - line comment!!
+*/
+
+console.log(typeof true);
+console.log(typeof 'true');
+console.log(typeof 22);
+
+// dont use let variable for re-assigning a new value
+
+
+// Basic Operators
+
+const now = 2020;
+const ageAli = now - 15;
+console.log(ageAli);
+console.log(ageAli * 2 / 2, ageAli ** 3);
+// x++; // x = x + 1
+
+// comparison operators 
+console.log(now > 2019) // true
